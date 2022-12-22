d4fa5240c54a4b511009dc076e3562975886e64a
daekyu.park <mamma1234@gmail.com>
2022-12-22 16:53:35 +0900

    update

--- app/routes/articles/$id.jsx
+++ app/routes/articles/$id.jsx
@@ -1,6 +1,22 @@
 import { useParams } from '@remix-run/react';
+import { Link, Outlet } from '@remix-run/react';
 
 export default function Article() {
   const params = useParams();
-  return <div>게시글 ID: {params.id}</div>;
+        return <div>
+        <div>게시글 ID: {params.id}</div>
+      <Outlet />
+      <hr />
+      <ul>
+        <li>
+          <Link to="/articles/1/actors">Actors 1</Link>
+        </li>
+        <li>
+          <Link to="/articles/2/actors">Actors 2</Link>
+        </li>
+        <li>
+          <Link to="/articles/3/actors">Actors 3</Link>
+        </li>
+      </ul>
+    </div>
 }
---
+++ app/routes/articles/$id/actors.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Actors() {
+    return (
+    <div>
+      Actors
+    </div>
+  );
+}
